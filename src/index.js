973f40f0142781ce7abed87f9c97be9c6ec607dd
antoinequellier <antoinecquellier@gmail.com>
2019-04-05 15:46:27 +0100

    add 2 comments div

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Faker from 'faker';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div class="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="" className="autor">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">
+            Nice blog post!
+          </div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="" className="autor">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">
+            Nice blog post!
+          </div>
+        </div>
+      </div>
+
       <div className="comment">
         <a href="/" className="avatar">
           <img src={faker.image.avatar()} alt="avatar"/>
