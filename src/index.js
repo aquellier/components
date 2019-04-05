7a327e0a917deda67c5091e040bd952848611fd6
antoinequellier <antoinecquellier@gmail.com>
2019-04-05 17:35:43 +0100

    dynamic avatar with props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45pm" content="Hey this is my comment"/>
-      <CommentDetail author="Alex" timeAgo="Today at 4:45pm" content="Hey this is my comment"/>
-      <CommentDetail author="JAne" timeAgo="Today at 4:45pm" content="Hey this is my comment"/>
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45pm"
+        content="Hey this is my comment"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 4:45pm"
+        content="Hey this is my comment"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="JAne"
+        timeAgo="Today at 4:45pm"
+        content="Hey this is my comment"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
